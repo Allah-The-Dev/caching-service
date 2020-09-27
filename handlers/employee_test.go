9005db4559045e44b378959fcf8550105b7d466d
Neelesh Pandey <neelesh.pandey@infosys.com>
2020-09-28 00:55:21 +0530

    benchmark testing for all 3 CRUD APIs

--- handlers/employee_test.go
+++ handlers/employee_test.go
@@ -2,88 +2,68 @@ package handlers
 
 import (
 	"bytes"
-	"encoding/json"
-	"io/ioutil"
+	"caching-service/config"
+	"caching-service/data"
 	"log"
-	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
 
-func sendGetRequest(client *http.Client, addr string) {
-	res, err := client.Get(addr)
-	if err != nil {
+var testLogger = log.New(os.Stdout, "emp : ", log.LstdFlags)
+
+func TestMain(m *testing.M) {
+	//set env vars
+	os.Setenv("MONGODB_SERVER", "")
+	os.Setenv("MONGODB_ADMINUSERNAME", "")
+	os.Setenv("MONGODB_ADMINPASSWORD", "")
+	os.Setenv("REDIS_SERVER", "")
+	os.Setenv("REDIS_PORT", "6379")
+	os.Setenv("KAFKA_SERVER", "")
+
+	config.InitializeAppConfig()
+
+	if err := data.InitializeMongoClient(); err != nil {
 		panic(err)
 	}
 
-	if res.StatusCode != 200 {
-		panic("request failed")
-	}
+	data.InitializeRedisClientPool()
 
-	_, err = ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	err = res.Body.Close()
-	if err != nil {
-		panic(err)
-	}
+	os.Exit(m.Run())
 }
 
-func sendPostRequest(client *http.Client, addr string, body map[string]string) {
+func BenchmarkGetEmployees(b *testing.B) {
 
-	jsonValue, _ := json.Marshal(body)
+	r := httptest.NewRequest("GET", "/api/v1/employee", nil)
 
-	resp, err := http.Post(addr, "application/json", bytes.NewBuffer(jsonValue))
-	if err != nil {
-		panic(err)
-	}
-
-	if resp.StatusCode != 200 {
-		panic("request failed")
-	}
-
-	_, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	err = resp.Body.Close()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func BenchMarkGetEmployees(b *testing.B) {
-
-	client := &http.Client{}
-	b.ResetTimer()
+	emp := &Employee{testLogger}
 
 	for i := 0; i < b.N; i++ {
-		sendGetRequest(client, "http://127.0.0.1:8080/api/v1/employee")
+		w := httptest.NewRecorder()
+		emp.GetEmployees(w, r)
 	}
 }
 
-func BenchMarkGetEmploye(b *testing.B) {
+func BenchmarkGetEmployee(b *testing.B) {
 
-	r := httptest.NewRequest("GET", "api/v1/employee", nil)
-	logger = log.New(os.Stdout, "employee-api : ", log.LstdFlags)
-	emp := &Employee{logger}
+	r := httptest.NewRequest("GET", "/api/v1/employee/foo", nil)
+
+	emp := &Employee{testLogger}
+
 	for i := 0; i < b.N; i++ {
 		w := httptest.NewRecorder()
 		emp.GetEmployee(w, r)
 	}
 }
 
-func BenchMarkPostEmployee(b *testing.B) {
+func BenchmarkPostEmployee(b *testing.B) {
 
-	client := &http.Client{}
-	b.ResetTimer()
+	r := httptest.NewRequest("POST", "/api/v1/employee", bytes.NewBuffer([]byte(`{"name": "foo", "unit": "bar"}`)))
+
+	emp := &Employee{testLogger}
 
 	for i := 0; i < b.N; i++ {
-		reqBody := map[string]string{"name": "foo", "unit": "bar"}
-		sendPostRequest(client, "http://127.0.0.1:8080/api/v1/employee", reqBody)
+		w := httptest.NewRecorder()
+		emp.GetEmployee(w, r)
 	}
 }
